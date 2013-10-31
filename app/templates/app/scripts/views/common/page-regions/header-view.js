6b90ebf8328cf5a73d0444705e2743b604814fd8
kongchangzhu <kongchangzhu@beisen.com>
2013-10-31 18:27:36 +0800

    header-view: setActive

--- app/templates/app/scripts/views/common/page-regions/header-view.js
+++ app/templates/app/scripts/views/common/page-regions/header-view.js
@@ -9,6 +9,18 @@ define(['talent','templates/common'], function(Talent, jst) {
 		/** @lends HeaderView.prototype */
 	{
 		template: jst['common/page-regions/header']
+		,initialize: function() {
+			Talent.app.vent.on('route',this.setActive, this);
+		}
+		,onRender: function() {
+			this.setActive();
+		}
+		,setActive: function() {
+			Talent.app.request('history:getFragments').done(_.bind(function(fragments) {
+				this.$el.find('.navbar li').removeClass('active');
+				this.$el.find('li.'+fragments[0]).addClass('active');
+			},this));
+		}
 	});
 
 });
