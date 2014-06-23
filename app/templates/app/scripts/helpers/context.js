48305d0b25508d6ac2be573d700d7d09f4f4dfbc
kongchangzhu <kongchangzhu@beisen.com>
2014-06-23 12:00:51 +0800

    remove BSGlobal.apiPath & getWebUrl method

--- app/templates/app/scripts/helpers/context.js
+++ app/templates/app/scripts/helpers/context.js
@@ -3,9 +3,6 @@
  */
 define(['talent'], function(Talent) {
 
-	// all ajax apis have same domain except jsonp call
-	var apiServer = BSGlobal.apiPath;
-
 	var localContext = {
 		/**
 		 * 获取租户信息
@@ -32,45 +29,6 @@ define(['talent'], function(Talent) {
 			var staticServer = BSGlobal.staticPath;
 			return staticServer + "/" + url;
 		},
-		/**
-		 * 获取后台接口的完整路径
-		 * @return
-		 * http://recruit.tms.beisen.com/Rercruting/Jobs
-		 */
-		getApiUrl: function(url){
-			var uid = this.getUserInfo().Id;
-			var tid = this.getTenantInfo().Id;
-			return apiServer + "/api/v1/"+tid+"/"+uid+"/" + url;
-		},
-		/**
-		 * 获取后台接口的域名部分
-		 * @return
-		 * http://webapi.tita.com
-		 */
-		getApiHost: function(){
-			return apiServer;
-		},
-		/**
-		 * 获取后台JSONP接口的完整路径
-		 * @return
-		 * http://recruit.tms.beisen.com/Rercruting/Jobs
-		 */
-		getWebUrl: function(url){
-			var http = "http://www.";
-			if(BSGlobal.env == 'Testing'){
-				http = "http://qa.";
-			}
-			return http + BSGlobal.webPath+"/" + url;
-		},
-		/**
-		 * 获取登录URL
-		 * @return
-		 * http://tms.beisen.com/
-		 */
-		getLoginUrl: function() {
-			var url = this.getWebUrl("Account/LogIn");
-			return url + "?ReturnUrl=" + encodeURIComponent(location.href);
-		}
 		,getLoading: function(){
 			var url = this.getStaticUrl('images/load_m.gif');
 			var html = [
